021733350ceea89215ec7ce5a2dffb2850debc96
Jeff <github@314ghz.com>
2018-11-13 21:03:15 -0600

    Initial import of project from https://start.spring.io/

---
+++ webstarter/src/main/java/com/codeontheweb/webstarter/WebstarterApplication.java
@@ -0,0 +1,12 @@
+package com.codeontheweb.webstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebstarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebstarterApplication.class, args);
+	}
+}
