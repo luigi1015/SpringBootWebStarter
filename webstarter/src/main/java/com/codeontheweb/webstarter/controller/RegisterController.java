565cebc70312c099c5b93c8db42d013a6709622e
Jeff <github@314ghz.com>
2018-11-18 17:18:39 -0600

    Updated the registration a little.

--- webstarter/src/main/java/com/codeontheweb/webstarter/controller/RegisterController.java
+++ webstarter/src/main/java/com/codeontheweb/webstarter/controller/RegisterController.java
@@ -1,9 +1,15 @@
 package com.codeontheweb.webstarter.controller;
 
+import java.util.UUID;
+
+import javax.validation.ConstraintViolationException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.TransactionSystemException;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -35,11 +41,26 @@ public class RegisterController
 	@PostMapping("/register")
 	public String processRegistrationForm( @ModelAttribute AppUser user, Model model )
 	{
-		//TODO: Put in logic to register the user.
 		//TODO: Add in HTTPS
+		//TODO: Put it some more security
 		logger.info("Username: {}", user.getUsername());
-		model.addAttribute("errorMessage", "Registration logic hasn't been implemented yet");
-		model.addAttribute("user", new AppUser());
-		return "register";
+		String password = user.getPassword();
+		user.setPassword(bCryptPasswordEncoder.encode(password));
+		user.setEnabled(true);
+		user.setId(UUID.randomUUID().toString());
+		try
+		{
+			userService.saveUser(user);
+		}
+		catch( TransactionSystemException | DataIntegrityViolationException ex )
+		{
+			//TODO: Put in a better error.
+			model.addAttribute("errorMessage", "Error registering the user, please verify that you entered in valid information.");
+			user.setPassword(password);//Set the password back to what the user entered.
+			model.addAttribute("user", user);
+			return "register";
+		}
+		model.addAttribute("regularMessage", "User has been registered.");
+		return "redirect:/home";
 	}
 }
